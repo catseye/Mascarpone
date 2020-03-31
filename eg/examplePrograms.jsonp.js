97ec02c4e886063eb7c0771ebd6660e77e0cb59a
Chris Pressey <cpressey@catseye.tc>
2020-03-31 16:37:59 +0100

    Add some example programs.

---
+++ eg/examplePrograms.jsonp.js
@@ -0,0 +1,10 @@
+examplePrograms = [
+    {
+        "contents": "v['[/''/']v*]v*'?<^'p?!.\n",
+        "filename": "capture.mascarpone"
+    },
+    {
+        "contents": "[o[ll]eh].........\n",
+        "filename": "hello.mascarpone"
+    }
+];
